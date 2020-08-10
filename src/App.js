a0ed772939333b40933ce44f7ed98593075444d4
shaima <shaima.shalbi@esprit.tn>
2020-08-10 13:58:35 +0200

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import  {DISHES} from './shared/dishes';
+
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+      dishes: DISHES
+    };
+
+  }
+
+
   render(){
 
     return (
@@ -16,7 +31,7 @@ class App extends Component {
             </NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
 

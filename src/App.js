9f48fb4683ab1ba0450a988460ed7724fbf99442
shaima <shaima.shalbi@esprit.tn>
2020-08-24 22:03:36 +0200

    1 assignement

--- src/App.js
+++ src/App.js
@@ -1,43 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
-import Menu from './components/MenuComponent';
-import  {DISHES} from './shared/dishes';
-
-
+import { Navbar, NavbarBrand } from 'reactstrap'
+import Menu from './components/MenuComponent'
+import { DISHES } from './shared/dishes'
 
 class App extends Component {
 
-  constructor(props){
-    super(props);
+  constructor(props) {
+    super(props)
 
     this.state = {
-
       dishes: DISHES
-    };
-
+    }
   }
 
-
-  render(){
-
+  render() {
     return (
-      <div >
-        <Navbar dark color="primary">
-          <div className="container"> 
-            <NavbarBrand href="/">
-              Ristorente Con Fusion
-            </NavbarBrand>
+      <div className="App">
+        <Navbar dark color='primary'>
+          <div className = 'container'>
+            <NavbarBrand href='/'> Test </NavbarBrand>
           </div>
         </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Menu dishes = { this.state.dishes }/>
       </div>
     );
-
-
   }
-  
 }
 
 export default App;
